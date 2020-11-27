0f67a893f63e4c224244cbf211f65990bab0a105
Alfonso Jari <alfonso.jari@gmail.com>
2020-11-26 18:26:06 -0600

    Fixed code-coverage

--- dist/ProyectoIMC/main.js
+++ dist/ProyectoIMC/main.js
@@ -224,12 +224,12 @@ class UiComponent {
     ngOnInit() {
     }
     cambioH() {
-        if (this.mujer = true) {
+        if (this.mujer) {
             this.mujer = false;
         }
     }
     cambioM() {
-        if (this.hombre = true) {
+        if (this.hombre) {
             this.hombre = false;
         }
     }
@@ -436,33 +436,26 @@ function clasificacion(imc) {
     if (imc < 18.5) {
         return ('Desnutricion');
     }
-    if (imc > 18.4 && imc < 25)
+    if (imc > 18.4 && imc < 25) {
         return "Normal";
-    {
     }
-    if (imc > 24.9 && imc < 27)
+    if (imc > 24.9 && imc < 27) {
         return "Sobre peso grado 1";
-    {
     }
-    if (imc > 26.9 && imc < 30)
+    if (imc > 26.9 && imc < 30) {
         return "Sobre peso grado 2";
-    {
     }
-    if (imc > 29.9 && imc < 35)
+    if (imc > 29.9 && imc < 35) {
         return "Obesidad tipo 1";
-    {
     }
-    if (imc > 34.9 && imc < 40)
+    if (imc > 34.9 && imc < 40) {
         return "Obesidad tipo 2";
-    {
     }
-    if (imc > 39.9 && imc < 50)
+    if (imc > 39.9 && imc < 50) {
         return "Obesidad tipo 3 Morbida";
-    {
     }
-    if (imc > 49.9)
+    else {
         return "Obesidad tipo 4 Extrema";
-    {
     }
 }
 
